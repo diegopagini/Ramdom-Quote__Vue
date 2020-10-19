750df7d9348bd607671848b2989c998cd4cc4885
unknown <diegopagini777@gmail.com>
2020-10-19 13:55:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+new Vue({
+  el: '#app',
+
+  data() {
+    return {
+      messages: [
+        {
+          message: 'Eighty percent of success is showing up.',
+          author: '- Woody Allen'
+        },
+        {
+          message: 'Start where you are. Use what you have. Do what you can.',
+          author: '- Arthur Ashe'
+        },
+        {
+          message: 'It is not what you do for your children, but what you have taught them to do for themselves, that will make them successful human beings.',
+          author: '- Ann Landers'
+        },
+        {
+          message: 'You can’t use up creativity. The more you use, the more you have.',
+          author: '- Maya Angelou'
+        },
+        {
+          message: 'We must balance conspicuous consumption with conscious capitalism.',
+          author: '- Kevin Kruse'
+        },
+        {
+          message: 'Whatever you can do, or dream you can, begin it. Boldness has genius, power and magic in it.',
+          author: '- Johann Wolfgang von Goethe'
+        },
+        {
+          message: 'Limitations live only in our minds. But if we use our imaginations, our possibilities become limitless.',
+          author: '- Jamie Paolinetti'
+        },
+        {
+          message: 'It’s not the years in your life that count. It’s the life in your years.',
+          author: '- Abraham Lincoln'
+        },
+
+
+      ]
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  }
+
+});
