1caa50753c2b5820cee566b4a705d9b0dcb77a0f
unknown <diegopagini777@gmail.com>
2020-10-19 15:13:17 -0300

    style ready

--- app.js
+++ app.js
@@ -3,65 +3,65 @@ new Vue({
 
   data() {
     return {
-      messages: [
+      quotes: [
         {
-          message: 'Eighty percent of success is showing up.',
+          quote: 'Eighty percent of success is showing up.',
           author: '- Woody Allen'
         },
         {
-          message: 'Start where you are. Use what you have. Do what you can.',
+          quote: 'Start where you are. Use what you have. Do what you can.',
           author: '- Arthur Ashe'
         },
         {
-          message: 'It is not what you do for your children, but what you have taught them to do for themselves, that will make them successful human beings.',
+          quote: 'It is not what you do for your children, but what you have taught them to do for themselves, that will make them successful human beings.',
           author: '- Ann Landers'
         },
         {
-          message: 'You can’t use up creativity. The more you use, the more you have.',
+          quote: 'You can’t use up creativity. The more you use, the more you have.',
           author: '- Maya Angelou'
         },
         {
-          message: 'We must balance conspicuous consumption with conscious capitalism.',
+          quote: 'We must balance conspicuous consumption with conscious capitalism.',
           author: '- Kevin Kruse'
         },
         {
-          message: 'Whatever you can do, or dream you can, begin it. Boldness has genius, power and magic in it.',
+          quote: 'Whatever you can do, or dream you can, begin it. Boldness has genius, power and magic in it.',
           author: '- Johann Wolfgang von Goethe'
         },
         {
-          message: 'Limitations live only in our minds. But if we use our imaginations, our possibilities become limitless.',
+          quote: 'Limitations live only in our minds. But if we use our imaginations, our possibilities become limitless.',
           author: '- Jamie Paolinetti'
         },
         {
-          message: 'It’s not the years in your life that count. It’s the life in your years.',
+          quote: 'It’s not the years in your life that count. It’s the life in your years.',
           author: '- Abraham Lincoln'
         },
         {
-          message: 'The two most important days in your life are the day you are born and the day you find out why.',
+          quote: 'The two most important days in your life are the day you are born and the day you find out why.',
           author: '- Mark Twain'
         },
         {
-          message: 'The question isn’t who is going to let me; it’s who is going to stop me.',
+          quote: 'The question isn’t who is going to let me; it’s who is going to stop me.',
           author: '- Ayn Rand'
         },
         {
-          message: 'Build your own dreams, or someone else will hire you to build theirs.',
+          quote: 'Build your own dreams, or someone else will hire you to build theirs.',
           author: '- Farrah Gray'
         },
         {
-          message: 'Ask and it will be given to you; search, and you will find; knock and the door will be opened for you.',
+          quote: 'Ask and it will be given to you; search, and you will find; knock and the door will be opened for you.',
           author: '- Jesus'
         },
         {
-          message: 'Certain things catch your eye, but pursue only those that capture the heart.',
+          quote: 'Certain things catch your eye, but pursue only those that capture the heart.',
           author: '- Ancient Indian Proverb'
         },
         {
-          message: 'I would rather die of passion than of boredom.',
+          quote: 'I would rather die of passion than of boredom.',
           author: '- Vincent van Gogh'
         },
         {
-          message: 'Life is what happens to you while you’re busy making other plans.',
+          quote: 'Life is what happens to you while you’re busy making other plans.',
           author: '- John Lennon'
         },
       ]
@@ -72,6 +72,9 @@ new Vue({
   },
 
   methods: {
+    showQuote() {
+      return this.quotes[0].quote
+    }
   }
 
 });
